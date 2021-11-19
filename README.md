239a3abf578266c0e3510c4b1ad3d5cd4f65512e
yclim95 <lyc950421@gmail.com>
2021-11-19 18:54:54 +0800

    Update to version 2021

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
 # Spring: Implementing Inversion of Control (IoC) using Console App
-
-## RESOURCE
-1. [Spring Basic](https://github.com/yclim95/spring-basic)
-2. [Spring Loose Coupling](https://github.com/yclim95/spring-loose-coupling)
-3. [Spring IoC Concept](https://github.com/yclim95/spring-ioc-fundamental)
-4. [Spring MVC](https://github.com/yclim95/Spring-MVC)
-5. [Spring CRUD](https://github.com/yclim95/Spring-CRUD)
-6. [Spring JPA](https://github.com/yclim95/Spring-JPA)
--- src/main/java/lithan/training/javawebapp/App.java
+++ src/main/java/lithan/training/javawebapp/App.java
@@ -1,10 +1,11 @@
 package lithan.training.javawebapp;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
- * Hello world!
+ * Spring IoC
  *
  */
 public class App 
@@ -13,7 +14,10 @@ public class App
     {
         System.out.println( "RST: Assignment01 - Spring IoC" );
         ApplicationContext context = new ClassPathXmlApplicationContext("Bean.xml");
-        BillingService bilingService = (BillingService) context.getBean("billingService");
+        BillingService bilingService = (BillingService) context.getBean("billingServiceGPay");
         System.out.println(bilingService.toString());
+        bilingService = (BillingService) context.getBean("billingServiceCard");
+        System.out.println(bilingService.toString());
+        ((AbstractApplicationContext) context).close();
     }
 }
